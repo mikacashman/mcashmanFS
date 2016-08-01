deb70a5e0be35f338cfeafd17703b5a20e29979d
Mikaela Marie Cashman <mcashman2@unl.edu>
2016-08-01 15:50:32 -0400

    iteratation added

--- lib/mcashman_FS/mcashman_FSImpl.py
+++ lib/mcashman_FS/mcashman_FSImpl.py
@@ -77,7 +77,10 @@ class mcashman_FS:
 	Strains = [] #genome names
 	Genes = [] #gene names
 	Functions = [] #gene functions
+	Scores = [] #Scores
 	temp = 0
+	for i in range(0,len(pan['orthologs'])):
+		Scores.append(0)
 	for i in range(0,len(pan['genome_refs'])):
 		Strains.append(pan['genome_refs'][i])
 		print(Strains[i])
@@ -94,44 +97,117 @@ class mcashman_FS:
 	Index=[]
 	for i in range(0,len(pan['orthologs'])):
 		Index.append(i)
-	random.shuffle(Index)
 
-	### STEP 5 - Create Arff file
-	filename = self.scratch + "/weka.arff"
-	outfilename = self.scratch + "/weka.out"
-	arff = open(filename,"w+")
-	arff.write("@RELATION FS\n\n")
-	for i in range(0,len(pan['orthologs'])):
-		arff.write("@ATTRIBUTE " + Genes[i] + "{ON,OFF}\n")
-	arff.write("@ATTRIBUTE class {GROWTH,NO_GROWTH}\n")
-	arff.write("\n@data\n")
-	for i in range(0,len(pan['genome_refs'])):
-		for j in range(0,len(pan['orthologs'])):
-			if Data[j][i] == 1:
-				arff.write("ON,")
+	for k in range(0,100):	
+		random.shuffle(Index)
+
+		### STEP 5 - Create Arff file
+		filename = self.scratch + "/weka.arff"
+		outfilename = self.scratch + "/weka.out"
+		arff = open(filename,"w+")
+		arff.write("@RELATION FS\n\n")
+		for i in range(0,len(pan['orthologs'])):
+			arff.write("@ATTRIBUTE " + Genes[Index[i]] + "{ON,OFF}\n")
+		arff.write("@ATTRIBUTE class {GROWTH,NO_GROWTH}\n")
+		arff.write("\n@data\n")
+		for i in range(0,len(pan['genome_refs'])):
+			for j in range(0,len(pan['orthologs'])):
+				if Data[Index[j]][i] == 1:
+					arff.write("ON,")
+				else:
+					arff.write("OFF,")
+			if params['classes'][i] == 1:
+				arff.write("GROWTH\n")
 			else:
-				arff.write("OFF,")
-		if params['classes'][i] == 1:
-			arff.write("GROWTH\n")
-		else:
-			arff.write("NO_GROWTH\n") 
-	arff.close()
+				arff.write("NO_GROWTH\n") 
+		arff.close()
+		
+		### STEP 6 - Run in Weka FIX THIS LATER
+		os.system("java weka.classifiers.trees.J48 -t " + filename + " -T " + filename + " -i > " + outfilename) 
+		
+		### STEP temp - Print results to report
+		#create report
+		#report = "Weka output\n"
+		#with open(outfilename) as f:
+		#	lines = f.readlines()
+		#	report+=str(lines)
+		#f.close()
+		#reportObj = {
+		#	'objects_created':[],
+		#	'text_message':report
+		#}
+		#save report
+		#provenance = [{}]
+		#if 'provenance' in ctx:
+		#	provenance = ctx['provenance']
+		# add additional info to provenance here, in this case the input data object reference
+		#provenance[0]['input_ws_objects']=[workspace_name+'/'+pan['id']]
+		#report_info_list = None
+		#try:
+		#	report_info_list = wsClient.save_objects({
+		#		'workspace':workspace_name,
+		#		'objects':[
+		#		{
+		#			'type':'KBaseReport.Report',
+		#			'data':reportObj,
+		#			'name':'FS_report',
+		#			'meta':{},
+		#			'hidden':1, # important!  make sure the report is hidden
+		#			'provenance':provenance
+		#		}
+		#		]
+		#	})
+		#except:
+		#	exc_type, exc_value, exc_traceback = sys.exc_info()
+		#	lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+		#	orig_error = ''.join('    ' + line for line in lines)
+		#	raise ValueError('Error saving Report object to workspace:\n' + orig_error)
+		#report_info = report_info_list[0]
+		#print('saved report: ' + pformat(report_info))
+		
+		
+		### STEP 7 - Record results for all genes (metric equation)
+		#parse file and add 1 to cluster if present
+		#f = open(outfilename,'r')
+		#while True:
+		#	ch=f.read(1)
+		#	if not ch: break
+		with open(outfilename,'r') as f:
+			f = file(outfilename).read()
+			for word in f.split():
+				if word[0:7]=='cluster':
+					#print(word)
+					temp = Genes.index(word)
+					Scores[temp]+=1
+				elif word=='Number': break
+		
+		### STEP 8 - Repeat x times
+		
+
+	### STEP 9 - Compute final metrics and report
+	ScoreGene = zip(Scores,Genes)
+	sortedGenes = sorted(ScoreGene, key = lambda pair: pair[0], reverse=True)
 	
-	### STEP 6 - Run in Weka FIX THIS LATER
-	os.system("java weka.classifiers.trees.J48 -t " + filename + " -T " + filename + " -i > " + outfilename) 
-	
+	for i in range(0,len(Genes)):
+		if sortedGenes[i][0] == 0: break
+		print(sortedGenes[i][1] + ": " + str(sortedGenes[i][0]))
+	#[x for (y,x) in sorted(zip(Scores,Genes), key = lambda pair: pair[0])]
+
 	### STEP temp - Print results to report
 	#create report
-	report = "Weka output\n"
-	with open(outfilename) as f:
-		lines = f.readlines()
-		report+=str(lines)
-	f.close()
+	report = "Ordered Genes\n-----------------------------"
+	#with open(outfilename) as f:
+	#	lines = f.readlines()
+	#	report+=str(lines)
+	#f.close()
+	for i in range(0,len(Genes)):
+		if sortedGenes[i][0] == 0: break
+		report+=sortedGenes[i][1] + ": " + str(sortedGenes[i][0])
 	reportObj = {
 		'objects_created':[],
 		'text_message':report
 	}
-        #save report
+	#save report
 	provenance = [{}]
 	if 'provenance' in ctx:
 		provenance = ctx['provenance']
@@ -139,36 +215,36 @@ class mcashman_FS:
 	provenance[0]['input_ws_objects']=[workspace_name+'/'+pan['id']]
 	report_info_list = None
 	try:
-        	report_info_list = wsClient.save_objects({
-                	'workspace':workspace_name,
-                	'objects':[
+		report_info_list = wsClient.save_objects({
+			'workspace':workspace_name,
+			'objects':[
 			{
-                        	'type':'KBaseReport.Report',
-                        	'data':reportObj,
-                        	'name':'FS_report',
-                        	'meta':{},
-                         	'hidden':1, # important!  make sure the report is hidden
-                         	'provenance':provenance
-                        }
+				'type':'KBaseReport.Report',
+				'data':reportObj,
+				'name':'FS_report',
+				'meta':{},
+				'hidden':1, # important!  make sure the report is hidden
+				'provenance':provenance
+			}
 			]
 		})
 	except:
-        	exc_type, exc_value, exc_traceback = sys.exc_info()
-        	lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-        	orig_error = ''.join('    ' + line for line in lines)
-        	raise ValueError('Error saving Report object to workspace:\n' + orig_error)
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+		orig_error = ''.join('    ' + line for line in lines)
+		raise ValueError('Error saving Report object to workspace:\n' + orig_error)
 	report_info = report_info_list[0]
-	print('saved report: ' + pformat(report_info))
-	
-	
-	### STEP 7 - Record results for all genes (metric equation)
-	### STEP 8 - Repeat x times
-	### STEP 9 - Compute final metrics and report
+	#print('saved report: ' + pformat(report_info))
 
+
+	#print('Final Scores\n')
+	#for i in range(0,100):
+	#	print(Genes[i] + ': ' + str(Scores[i]))
+	
 	print('Size of Data: ' + str(len(Data)))
-	for i in range(0,4):
-		for j in range(0,len(pan['genome_refs'])):
-			print(str(Data[i][j]) + ' ') 
+	#for i in range(0,4):
+	#	for j in range(0,len(pan['genome_refs'])):
+	#		print(str(Data[i][j]) + ' ') 
 
 	print('Ready to return')
 	returnVal = {
